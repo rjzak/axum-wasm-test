0bf386ef8b707cdd69e909dc31ac6ad2657ac3c8
Richard Zak <richard@profian.com>
2022-06-06 17:49:43 -0400

    Different main for Wasi, or not
    Signed-off-by: Richard Zak <richard@profian.com>

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,17 @@ use axum::response::Html;
 use axum::routing::get;
 use std::env;
 
-#[tokio::main(flavor = "current_thread")]
+/// Ideally, `#[tokio::main]` will work regardless of target by
+/// determining that for Wasi, the flavor must be
+/// `tokio::runtime::current_thread`.
+#[cfg(not(target_arch = "wasm32"))]
+#[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    tracing_subscriber::fmt::init();
-
     #[cfg(not(target_arch = "wasm32"))]
     {
         use std::net::SocketAddr;
+
+        tracing_subscriber::fmt::init();
         let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
         tracing::debug!("listening on {}", addr);
         axum::Server::bind(&addr)
@@ -20,12 +24,29 @@ async fn main() -> anyhow::Result<()> {
     #[cfg(target_arch = "wasm32")]
     {
         use std::os::wasi::io::FromRawFd;
+
+        tracing_subscriber::fmt::init();
         let std_listener = unsafe { std::net::TcpListener::from_raw_fd(3) };
         std_listener.set_nonblocking(true).unwrap();
         axum::Server::from_tcp(std_listener)
             .unwrap()
             .serve(app().into_make_service()).await?;
     }
+
+    Ok(())
+}
+
+#[cfg(target_arch = "wasm32")]
+#[tokio::main(flavor = "current_thread")]
+async fn main() -> anyhow::Result<()> {
+    use std::os::wasi::io::FromRawFd;
+
+    tracing_subscriber::fmt::init();
+    let std_listener = unsafe { std::net::TcpListener::from_raw_fd(3) };
+    std_listener.set_nonblocking(true).unwrap();
+    axum::Server::from_tcp(std_listener)
+        .unwrap()
+        .serve(app().into_make_service()).await?;
 
     Ok(())
 }
